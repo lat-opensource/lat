lat-opensource/lat

commit 041a54d2b0b6c923708a4b7b629b4d9010839947

Fix for New World system build.
修正在ABI 2.0（新世界）系统环境下编译失败的问题。

include/qemu/osdep.h
@@ -27,15 +27,22 @@
 #ifndef QEMU_OSDEP_H
 #define QEMU_OSDEP_H
 
+#include "config-host.h"
+
 /* arch dependent signal context retriev macros */
 #if defined(__mips__)
 #define UC_GR(uc) ((uc)->uc_mcontext.gregs)
 #define UC_PC(uc) ((uc)->uc_mcontext.pc)
 #elif defined(__loongarch__)
+#ifdef CONFIG_LOONGARCH_NEW_WORLD
 #define UC_GR(uc) ((uc)->uc_mcontext.__gregs)
 #define UC_PC(uc) ((uc)->uc_mcontext.__pc)
-#define UC_FREG(uc) ((uc)->uc_mcontext.__fpregs)
-#define UC_FCSR(uc) ((uc)->uc_mcontext.__fcsr)
+#else
+    #define UC_GR(uc) ((uc)->uc_mcontext.sc_regs)
+    #define UC_PC(uc) ((uc)->uc_mcontext.sc_pc)
+    #define UC_FREG(uc) ((uc)->uc_mcontext.__fpregs)
+    #define UC_FCSR(uc) ((uc)->uc_mcontext.__fcsr)
+#endif
 #else
 #error "unknown arch"
 #endif

accel/tcg/translate-all.c
@@ -4607,6 +4607,7 @@ int shared_private_interpret(siginfo_t *info, ucontext_t *uc)
         *(int64_t *)(mem_addr + 24) = UC_GET_LASX(&extctx, fd, 3, int64_t);
         goto end;
 #endif
+#ifndef CONFIG_LOONGARCH_NEW_WORLD
     case 0xc0:
         if (inst & (1<<21)) {
             /*VLDREPL.W*/
@@ -4634,6 +4635,7 @@ int shared_private_interpret(siginfo_t *info, ucontext_t *uc)
             *((int8_t *)&UC_FREG(uc)[fd].__val32[i] + 3) = *(int8_t *)mem_addr;
         }
         goto end;
+#endif
     case 0xb8: /* LDL.W */
     case 0xb9: /* LDR.W */
     case 0xba: /* LDL.D */

include/loongarch-extcontext.h
@@ -5,6 +5,8 @@
  */
 #ifndef LOONGARCH_EXTCONTEXT_H
 #define LOONGARCH_EXTCONTEXT_H
+#include "config-host.h"
+
 #if defined(CONFIG_LOONGARCH_NEW_WORLD) && defined(__loongarch__)
 #include "qemu/osdep.h"
 #include "asm/sigcontext.h"

target/i386/latx/context/myalign.c
@@ -1,3 +1,4 @@
+#include "config-host.h"
 #include "lsenv.h"
 #include "myalign.h"
 #include "elfloader.h"

target/i386/latx/context/wrappedlibc.c
@@ -1,3 +1,4 @@
+#include "config-host.h"
 #define _LARGEFILE_SOURCE 1
 #define _FILE_OFFSET_BITS 64
 #include <stdlib.h>

target/i386/latx/include/generated/wrappedlibctypes.h
@@ -4,6 +4,8 @@
 #ifndef __wrappedlibcTYPES_H_
 #define __wrappedlibcTYPES_H_
 
+#include "config-host.h"
+
 #ifndef LIBNAME
 #error You should only #include this file inside a wrapped*.c file
 #endif

target/i386/latx/include/library_list.h
@@ -2,6 +2,8 @@
 #error Nope
 #endif
 
+#include "config-host.h"
+
 GO("libEGL.so.1", libegl)
 #ifndef CONFIG_LOONGARCH_NEW_WORLD
 GO("libdl.so.2", libdl)

target/i386/latx/include/wrappedlibc_private.h
@@ -1,3 +1,4 @@
+#include "config-host.h"
 #if !(defined(GO) && defined(GOM) && defined(GO2) && defined(DATA) && defined(GOS))
 #error Meh...
 #endif

commit 159bebe2d23e8658d028b9944114367085d34eb1

LATX, fix: Fix compile error caused by commit `041a54d2b0 Fix for New World system build`
The correct definition for `uc_mcontext` is provided by `/usr/include/loongarch64-linux-gnu/sys/ucontext.h`.

Signed-off-by: Hanlu Li <heuleehanlu@gmail.com>

include/qemu/osdep.h
@@ -34,14 +34,11 @@
 #define UC_GR(uc) ((uc)->uc_mcontext.gregs)
 #define UC_PC(uc) ((uc)->uc_mcontext.pc)
 #elif defined(__loongarch__)
-#ifdef CONFIG_LOONGARCH_NEW_WORLD
 #define UC_GR(uc) ((uc)->uc_mcontext.__gregs)
 #define UC_PC(uc) ((uc)->uc_mcontext.__pc)
-#else
-    #define UC_GR(uc) ((uc)->uc_mcontext.sc_regs)
-    #define UC_PC(uc) ((uc)->uc_mcontext.sc_pc)
-    #define UC_FREG(uc) ((uc)->uc_mcontext.__fpregs)
-    #define UC_FCSR(uc) ((uc)->uc_mcontext.__fcsr)
+#ifndef CONFIG_LOONGARCH_NEW_WORLD
+ #define UC_FREG(uc) ((uc)->uc_mcontext.__fpregs)
+ #define UC_FCSR(uc) ((uc)->uc_mcontext.__fcsr)
 #endif
 #else
 #error "unknown arch"