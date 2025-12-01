4e3d73793e5580d997c4d3c80d4789d5a2c3217d
liuchaoyi <liuchaoyi@loongson.cn>
2025-12-01 10:08:41 +0800

    LATX, opt: Replace reading the fcsr register with comparing the floating-point value against the maximum fixed-point value supported by the target register.
    Thanks to phorcys for the idea.
    
    for example:
    			cvtps2pi		vftint.w.s
    	normal		   --		   	   --
    	+/- nan	        0x80000000 raise V	0x0 raise V
    	+overflow	0x80000000 raises V	0x7FFFFFFF raises V
    	-overflow	0x80000000 raises V	0x80000000 raises V
    	-underflow	0x0 raises I	        0x0 raises I
    	+underflow	0x0 raises I	        0x0 raises I
    
    	float value	hex value	type
    	2147483648.0f	0x4f000000	(_Float32)
    	2147483520.0f	0x4effffff	(_Float32)
    so we can use fcmp.cule.s mask, 0x4f000000, src to get mask in one shot.
    then use vbitsel.v dest,dest,0x80000000,mask to combine 0x80000000 into vftint.w.s dest operand.
    
    Co-authored-by: phorcys <phorcys@126.com>
    Signed-off-by: liuchaoyi <liuchaoyi@loongson.cn>

--- target/i386/latx/include/latx-options.h
+++ target/i386/latx/include/latx-options.h
@@ -77,6 +77,7 @@ extern int option_monitor_shared_mem;
 extern int option_shadow_file;
 extern int option_smc_opt;
 extern int option_set_rounding_opt;
+extern int option_cvt_opt;
 static inline int latx_smc_default(void) { return 0x2 | 0x4; }
 static inline int latx_smc_inv_page(void) { return option_smc_opt == 0; }
 static inline int latx_smc_inv_tb(void) { return option_smc_opt != 0; }
