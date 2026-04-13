cea139022266c2ae0056f4ce14c5c3f0b912549f
Wenqiang Wei <weiwenqiang@mail.ustc.edu.cn>
2026-04-13 14:51:42 +0800

    LATX, opt: SMC TB reload
    Track TBs invalidated by target_mprotect and reload unmodified ones
    on subsequent page execution, reducing re-translation overhead.

--- target/i386/latx/latx-options.c
+++ target/i386/latx/latx-options.c
@@ -62,6 +62,7 @@ int option_debug_lative;
 int option_aot;
 int option_load_aot;
 int option_aot_wine;
+int option_smc_reload;
 int option_debug_aot;
 int option_imm_reg;
 int option_imm_rip;
@@ -238,6 +239,7 @@ void options_init(void)
     option_aot_wine = 0;
     option_debug_aot = 0;
 #endif
+    option_smc_reload = 0;
 #ifdef CONFIG_LATX_IMM_REG
     option_imm_reg = 0;
     option_imm_rip = 0;
