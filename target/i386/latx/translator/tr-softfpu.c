lat-opensource/lat

commit fe5993f639f62d73ed11981c86040aa9f9006e16

Add avx support

target/i386/latx/translator/tr-softfpu.c
@@ -460,7 +460,7 @@ static void gen_softfpu_helper2m_16u(ADDR func, IR2_OPND mem_opnd)
     /* load func_addr and jmp */
     save_imm_cache();
     la_jirl(ra_ir2_opnd, func_addr_opnd, 0);
-    restore_imm_cache();    
+    restore_imm_cache();
 }
 
 __attribute__((unused))
@@ -3140,6 +3140,76 @@ static bool translate_fyl2xp1_softfpu(IR1_INST *pir1)
     return true;
 }
 
+#ifdef CONFIG_LATX_AVX_OPT
+static bool translate_xgetbv_softfpu(IR1_INST *pir1)
+{
+    IR2_OPND ecx_opnd = ra_alloc_gpr(ecx_index);
+    gen_softfpu_helper2m_ptr((ADDR)helper_xgetbv, ecx_opnd);
+    IR2_OPND temp_low = ra_alloc_itemp();
+    IR2_OPND temp_high = ra_alloc_itemp();
+    la_bstrpick_d(temp_low, a0_ir2_opnd, 31, 0);
+    la_bstrpick_d(temp_high, a0_ir2_opnd, 63, 32);
+    la_st_d(temp_low, env_ir2_opnd, lsenv_offset_of_gpr(lsenv, R_EAX));
+    la_st_d(temp_high, env_ir2_opnd, lsenv_offset_of_gpr(lsenv, R_EDX));
+    return true;
+}
+
+static bool translate_xsetbv_softfpu(IR1_INST *pir1)
+{
+    IR2_OPND eax_opnd = ra_alloc_gpr(eax_index);
+    IR2_OPND ecx_opnd = ra_alloc_gpr(ecx_index);
+    IR2_OPND edx_opnd = ra_alloc_gpr(edx_index);
+    IR2_OPND temp_rfbm = ra_alloc_itemp();
+
+    la_bstrins_d(temp_rfbm, eax_opnd, 31, 0);
+    la_bstrins_d(temp_rfbm, edx_opnd, 63, 32);
+    gen_softfpu_helper3_ll((ADDR)helper_xsetbv, ecx_opnd, temp_rfbm);
+    return true;
+}
+
+static bool translate_xsave_softfpu(IR1_INST *pir1)
+{
+    IR2_OPND eax_opnd = ra_alloc_gpr(eax_index);
+    IR2_OPND edx_opnd = ra_alloc_gpr(edx_index);
+    IR2_OPND temp_rfbm = ra_alloc_itemp();
+    IR1_OPND *opnd0 = ir1_get_opnd(pir1, 0);
+    IR2_OPND mem_opnd = convert_mem_no_offset(opnd0);
+
+    la_bstrins_d(temp_rfbm, eax_opnd, 31, 0);
+    la_bstrins_d(temp_rfbm, edx_opnd, 63, 32);
+    gen_softfpu_helper3_ll((ADDR)helper_xsave, mem_opnd, temp_rfbm);
+    return true;
+}
+
+static bool translate_xsaveopt_softfpu(IR1_INST *pir1)
+{
+    IR2_OPND eax_opnd = ra_alloc_gpr(eax_index);
+    IR2_OPND edx_opnd = ra_alloc_gpr(edx_index);
+    IR2_OPND temp_rfbm = ra_alloc_itemp();
+    IR1_OPND *opnd0 = ir1_get_opnd(pir1, 0);
+    IR2_OPND mem_opnd = convert_mem_no_offset(opnd0);
+
+    la_bstrins_d(temp_rfbm, eax_opnd, 31, 0);
+    la_bstrins_d(temp_rfbm, edx_opnd, 63, 32);
+    gen_softfpu_helper3_ll((ADDR)helper_xsaveopt, mem_opnd, temp_rfbm);
+    return true;
+}
+
+static bool translate_xrstor_softfpu(IR1_INST *pir1)
+{
+    IR2_OPND eax_opnd = ra_alloc_gpr(eax_index);
+    IR2_OPND edx_opnd = ra_alloc_gpr(edx_index);
+    IR2_OPND temp_rfbm = ra_alloc_itemp();
+    IR1_OPND *opnd0 = ir1_get_opnd(pir1, 0);
+    IR2_OPND mem_opnd = convert_mem_no_offset(opnd0);
+
+    la_bstrins_d(temp_rfbm, eax_opnd, 31, 0);
+    la_bstrins_d(temp_rfbm, edx_opnd, 63, 32);
+    gen_softfpu_helper3_ll((ADDR)helper_xrstor, mem_opnd, temp_rfbm);
+    return true;
+}
+#endif
+
 TRANS_FPU_WRAP_GEN(wait);
 TRANS_FPU_WRAP_GEN(f2xm1);
 TRANS_FPU_WRAP_GEN(fabs);
@@ -3224,3 +3294,10 @@ TRANS_FPU_WRAP_GEN(fxsave);
 TRANS_FPU_WRAP_GEN(fxtract);
 TRANS_FPU_WRAP_GEN(fyl2x);
 TRANS_FPU_WRAP_GEN(fyl2xp1);
+#ifdef CONFIG_LATX_AVX_OPT
+TRANS_FPU_WRAP_GEN_NO_PROLOGUE(xgetbv);
+TRANS_FPU_WRAP_GEN(xsetbv);
+TRANS_FPU_WRAP_GEN(xsave);
+TRANS_FPU_WRAP_GEN(xsaveopt);
+TRANS_FPU_WRAP_GEN(xrstor);
+#endif

configure
@@ -239,7 +239,7 @@ fi
 #   meson will not detect the ccache.
 # So we need to add 'ccache' here, instead of use meson to detect.
 # Also, meson will do double check, if 'ccache' not exsits, meson
-#   will delete 'ccache' and remain 'cc'. 
+#   will delete 'ccache' and remain 'cc'.
 ccache=""
 if [ -x "$(command -v ccache)" ]; then
   ccache="ccache"
@@ -409,6 +409,7 @@ guest_base_zero="no"
 latx_capstone_git="yes"
 latx_atomic="no"
 latx_xcomisx_opt="no"
+latx_avx_opt="no"
 capstone_diet="yes"
 latx_syscall_tunnel="no"
 latx_tbmini_enable="yes"
@@ -1253,6 +1254,8 @@ for opt do
   --enable-latx-atomic) latx_atomic="yes"
   ;;
   --enable-latx-xcomisx-opt) latx_xcomisx_opt="yes"
+  ;;
+  --enable-latx-avx-opt) latx_avx_opt="yes"
   ;;
   --disable-capstone-diet) capstone_diet="no"
   ;;
@@ -5779,6 +5782,9 @@ if test "$latx" = "yes" ; then
   fi
   if test "$latx_xcomisx_opt" = "yes" ; then
   echo "CONFIG_LATX_XCOMISX_OPT=y" >> $config_host_mak
+  fi
+  if test "$latx_avx_opt" = "yes" ; then
+  echo "CONFIG_LATX_AVX_OPT=y" >> $config_host_mak
   fi
   if test "$capstone_diet" = "yes" ; then
     echo "CONFIG_CAPSTONE_DIET=y" >> $config_host_mak

target/i386/cpu.c
@@ -626,12 +626,21 @@ static void x86_cpu_vendor_words2str(char *dst, uint32_t vendor1,
           /* missing:
           CPUID_VME, CPUID_DTS, CPUID_SS, CPUID_HT, CPUID_TM, CPUID_PBE */
 
+#ifdef CONFIG_LATX_AVX_OPT
+#define TCG_EXT_FEATURES (CPUID_EXT_SSE3 | CPUID_EXT_PCLMULQDQ | \
+          CPUID_EXT_MONITOR | CPUID_EXT_SSSE3 | CPUID_EXT_CX16 | \
+          CPUID_EXT_SSE41 | CPUID_EXT_SSE42 | CPUID_EXT_POPCNT | \
+          CPUID_EXT_XSAVE | /* CPUID_EXT_OSXSAVE is dynamic */   \
+          CPUID_EXT_MOVBE | CPUID_EXT_AES | CPUID_EXT_HYPERVISOR | \
+          CPUID_EXT_RDRAND | CPUID_EXT_AVX | CPUID_EXT_FMA | CPUID_EXT_F16C )
+#else
 #define TCG_EXT_FEATURES (CPUID_EXT_SSE3 | CPUID_EXT_PCLMULQDQ | \
           CPUID_EXT_MONITOR | CPUID_EXT_SSSE3 | CPUID_EXT_CX16 | \
           CPUID_EXT_SSE41 | CPUID_EXT_SSE42 | CPUID_EXT_POPCNT | \
           CPUID_EXT_XSAVE | /* CPUID_EXT_OSXSAVE is dynamic */   \
           CPUID_EXT_MOVBE | CPUID_EXT_AES | CPUID_EXT_HYPERVISOR | \
           CPUID_EXT_RDRAND)
+#endif
           /* missing:
           CPUID_EXT_DTES64, CPUID_EXT_DSCPL, CPUID_EXT_VMX, CPUID_EXT_SMX,
           CPUID_EXT_EST, CPUID_EXT_TM2, CPUID_EXT_CID, CPUID_EXT_FMA,
@@ -654,21 +663,32 @@ static void x86_cpu_vendor_words2str(char *dst, uint32_t vendor1,
 #define TCG_EXT4_FEATURES 0
 #define TCG_SVM_FEATURES CPUID_SVM_NPT
 #define TCG_KVM_FEATURES 0
-
+#ifdef CONFIG_LATX_AVX_OPT
+#define TCG_7_0_EBX_FEATURES (CPUID_7_0_EBX_SMEP | CPUID_7_0_EBX_SMAP | \
+          CPUID_7_0_EBX_BMI1 | CPUID_7_0_EBX_BMI2 | CPUID_7_0_EBX_ADX | \
+          CPUID_7_0_EBX_PCOMMIT | CPUID_7_0_EBX_CLFLUSHOPT |            \
+          CPUID_7_0_EBX_CLWB | CPUID_7_0_EBX_MPX | CPUID_7_0_EBX_FSGSBASE | \
+          CPUID_7_0_EBX_ERMS | CPUID_7_0_EBX_AVX2 | CPUID_7_0_EBX_HLE)
+#else
 #define TCG_7_0_EBX_FEATURES (CPUID_7_0_EBX_SMEP | CPUID_7_0_EBX_SMAP | \
           CPUID_7_0_EBX_BMI1 | CPUID_7_0_EBX_BMI2 | CPUID_7_0_EBX_ADX | \
           CPUID_7_0_EBX_PCOMMIT | CPUID_7_0_EBX_CLFLUSHOPT |            \
           CPUID_7_0_EBX_CLWB | CPUID_7_0_EBX_MPX | CPUID_7_0_EBX_FSGSBASE | \
           CPUID_7_0_EBX_ERMS)
+#endif
           /* missing:
           CPUID_7_0_EBX_HLE, CPUID_7_0_EBX_AVX2,
           CPUID_7_0_EBX_INVPCID, CPUID_7_0_EBX_RTM,
           CPUID_7_0_EBX_RDSEED */
-
+#ifdef CONFIG_LATX_AVX_OPT
+#define TCG_7_0_ECX_FEATURES (CPUID_7_0_ECX_PKU | \
+          /* CPUID_7_0_ECX_OSPKE is dynamic */ \
+          CPUID_7_0_ECX_LA57)
+#else
 #define TCG_7_0_ECX_FEATURES (CPUID_7_0_ECX_PKU | \
           /* CPUID_7_0_ECX_OSPKE is dynamic */ \
           CPUID_7_0_ECX_LA57 | CPUID_7_0_ECX_PKS)
-
+#endif
 #define TCG_7_0_EDX_FEATURES 0
 #define TCG_7_1_EAX_FEATURES 0
 #define TCG_APM_FEATURES 0
@@ -1890,6 +1910,9 @@ static X86CPUDefinition builtin_x86_defs[] = {
             CPUID_MTRR | CPUID_CLFLUSH | CPUID_MCA |
             CPUID_PSE36,
         .features[FEAT_1_ECX] =
+#ifdef CONFIG_LATX_AVX_OPT
+            CPUID_EXT_AVX | CPUID_EXT_FMA | CPUID_EXT_XSAVE |
+#endif
 #ifdef CONFIG_LATX_SSSE3_SSE4
             CPUID_EXT_AES | CPUID_EXT_POPCNT | CPUID_EXT_SSE42 |
             CPUID_EXT_SSE41 | CPUID_EXT_CX16 | CPUID_EXT_SSSE3 |
@@ -1900,6 +1923,15 @@ static X86CPUDefinition builtin_x86_defs[] = {
             CPUID_EXT2_LM | CPUID_EXT2_SYSCALL | CPUID_EXT2_NX,
         .features[FEAT_8000_0001_ECX] =
             CPUID_EXT3_LAHF_LM | CPUID_EXT3_SVM,
+#ifdef CONFIG_LATX_AVX_OPT
+        .features[FEAT_7_0_EBX] =
+        CPUID_7_0_EBX_HLE |
+            CPUID_7_0_EBX_BMI1 |
+            CPUID_7_0_EBX_AVX2 |
+            CPUID_7_0_EBX_BMI2,
+        .features[FEAT_XSAVE] =
+            CPUID_XSAVE_XSAVEOPT | CPUID_XSAVE_XGETBV1,
+#endif
         .xlevel = 0x8000000A,
         .model_id = "LATX64 Virtual CPU @ 2.5GHz",
     },
@@ -2028,6 +2060,9 @@ static X86CPUDefinition builtin_x86_defs[] = {
         .features[FEAT_1_EDX] =
             PPRO_FEATURES,
         .features[FEAT_1_ECX] =
+#ifdef CONFIG_LATX_AVX_OPT
+            CPUID_EXT_AVX | CPUID_EXT_FMA | CPUID_EXT_XSAVE |
+#endif
 #ifdef CONFIG_LATX_SSSE3_SSE4
             CPUID_EXT_AES | CPUID_EXT_POPCNT | CPUID_EXT_SSE42 |
             CPUID_EXT_SSE41 | CPUID_EXT_CX16 | CPUID_EXT_SSSE3 |
@@ -2036,6 +2071,15 @@ static X86CPUDefinition builtin_x86_defs[] = {
             CPUID_EXT_SSE3,
         .features[FEAT_8000_0001_ECX] =
             CPUID_EXT3_LAHF_LM | CPUID_EXT3_SVM,
+#ifdef CONFIG_LATX_AVX_OPT
+        .features[FEAT_7_0_EBX] =
+        CPUID_7_0_EBX_HLE |
+            CPUID_7_0_EBX_BMI1 |
+            CPUID_7_0_EBX_AVX2 |
+            CPUID_7_0_EBX_BMI2,
+        .features[FEAT_XSAVE] =
+            CPUID_XSAVE_XSAVEOPT | CPUID_XSAVE_XGETBV1,
+#endif
         .xlevel = 0x80000004,
         .model_id = "LATX32 Virtual CPU @ 2.5GHz",
     },

target/i386/latx/include/optimize-config.h
@@ -107,6 +107,8 @@
 #ifdef _OPT_TESTING_
 #undef CONFIG_LATX_SYSCALL_TUNNEL
 #define CONFIG_LATX_SYSCALL_TUNNEL
+#undef CONFIG_LATX_AVX_OPT
+#define CONFIG_LATX_AVX_OPT
 #endif
 
 /**

target/i386/latx/include/translate.h
@@ -711,20 +711,416 @@ TRANS_FUNC_DEF(sha256msg1);
 TRANS_FUNC_DEF(sha256msg2);
 TRANS_FUNC_DEF(sha256rnds2);
 
+TRANS_FUNC_DEF(vpaddx);
+#ifdef CONFIG_LATX_AVX_OPT
+/* f16c */
+TRANS_FUNC_DEF(vcvtph2ps);
+TRANS_FUNC_DEF(vcvtps2ph);
+
+/* avx */
+TRANS_FUNC_DEF(vpclmulqdq);
+TRANS_FUNC_DEF(vtestpd);
+TRANS_FUNC_DEF(vtestps);
+TRANS_FUNC_DEF(vzeroall);
+TRANS_FUNC_DEF(vpmulhrsw);
+TRANS_FUNC_DEF(vphminposuw);
+TRANS_FUNC_DEF(vphsubsw);
+TRANS_FUNC_DEF(vphsubd);
+TRANS_FUNC_DEF(vphsubw);
+TRANS_FUNC_DEF(vpmaddubsw);
+TRANS_FUNC_DEF(vpmaddwd);
+TRANS_FUNC_DEF(vpmulhuw);
+TRANS_FUNC_DEF(vpmulhw);
+TRANS_FUNC_DEF(vpsignw);
+TRANS_FUNC_DEF(vpsignd);
+TRANS_FUNC_DEF(vpsignb);
+TRANS_FUNC_DEF(vrcpss);
+TRANS_FUNC_DEF(vrcpps);
+TRANS_FUNC_DEF(vrsqrtss);
+TRANS_FUNC_DEF(vrsqrtps);
+TRANS_FUNC_DEF(vpermilpd);
+TRANS_FUNC_DEF(vpermilps);
+TRANS_FUNC_DEF(vpermpx);
+TRANS_FUNC_DEF(vpsravd);
+TRANS_FUNC_DEF(vpsllvq);
+TRANS_FUNC_DEF(vpsllvd);
+TRANS_FUNC_DEF(vpmaskmovx);
+TRANS_FUNC_DEF(vpshufd);
+TRANS_FUNC_DEF(vpmuldq);
+TRANS_FUNC_DEF(vpinsrx);
+TRANS_FUNC_DEF(vpminxx);
+TRANS_FUNC_DEF(vpmaxxx);
+TRANS_FUNC_DEF(vpalignr);
+
+TRANS_FUNC_DEF(vmovsd);
+TRANS_FUNC_DEF(vmovntdqa);
+TRANS_FUNC_DEF(vmovlhps);
+TRANS_FUNC_DEF(vmovhpd);
+TRANS_FUNC_DEF(vmovhps);
+TRANS_FUNC_DEF(vmovhlps);
+TRANS_FUNC_DEF(vmovlpd);
+TRANS_FUNC_DEF(vmovlps);
+TRANS_FUNC_DEF(vmovq);
+TRANS_FUNC_DEF(vmovapd);
+TRANS_FUNC_DEF(vmovaps);
+TRANS_FUNC_DEF(vmovddup);
+TRANS_FUNC_DEF(vmovdqa);
+TRANS_FUNC_DEF(vmovdqu);
+TRANS_FUNC_DEF(vmovmskpd);
+TRANS_FUNC_DEF(vmovmskps);
+TRANS_FUNC_DEF(vmovntdq);
+TRANS_FUNC_DEF(vmovntpd);
+TRANS_FUNC_DEF(vmovntps);
+TRANS_FUNC_DEF(vmovshdup);
+TRANS_FUNC_DEF(vmovsldup);
+TRANS_FUNC_DEF(vmovss);
+//TRANS_FUNC_DEF(vmovsd);
+TRANS_FUNC_DEF(vmovd);
+//TRANS_FUNC_DEF(vmovq);
+TRANS_FUNC_DEF(vlddqu);
+TRANS_FUNC_DEF(vmovupd);
+TRANS_FUNC_DEF(vmovups);
+TRANS_FUNC_DEF(vmaskmovpx);
+TRANS_FUNC_DEF(vpmovmskb);
+TRANS_FUNC_DEF(vaddpd);
+TRANS_FUNC_DEF(vaddps);
+TRANS_FUNC_DEF(vaddsd);
+TRANS_FUNC_DEF(vaddss);
+TRANS_FUNC_DEF(vsubpd);
+TRANS_FUNC_DEF(vsubps);
+TRANS_FUNC_DEF(vsubsd);
+TRANS_FUNC_DEF(vsubss);
+TRANS_FUNC_DEF(vmulpd);
+TRANS_FUNC_DEF(vmulps);
+TRANS_FUNC_DEF(vmulsd);
+TRANS_FUNC_DEF(vmulss);
+TRANS_FUNC_DEF(vdivpd);
+TRANS_FUNC_DEF(vdivps);
+TRANS_FUNC_DEF(vdivsd);
+TRANS_FUNC_DEF(vdivss);
+TRANS_FUNC_DEF(vsqrtpd);
+TRANS_FUNC_DEF(vsqrtps);
+TRANS_FUNC_DEF(vsqrtsd);
+TRANS_FUNC_DEF(vsqrtss);
+TRANS_FUNC_DEF(vaddsubpd);
+TRANS_FUNC_DEF(vaddsubps);
+TRANS_FUNC_DEF(vhaddpd);
+TRANS_FUNC_DEF(vhaddps);
+TRANS_FUNC_DEF(vhsubpd);
+TRANS_FUNC_DEF(vhsubps);
+TRANS_FUNC_DEF(vandnpd);
+TRANS_FUNC_DEF(vandnps);
+TRANS_FUNC_DEF(vandpd);
+TRANS_FUNC_DEF(vandps);
+TRANS_FUNC_DEF(vorps);
+TRANS_FUNC_DEF(vorpd);
+TRANS_FUNC_DEF(vxorpd);
+TRANS_FUNC_DEF(vxorps);
+TRANS_FUNC_DEF(vmaxpx);
+TRANS_FUNC_DEF(vmaxsx);
+TRANS_FUNC_DEF(vminpx);
+TRANS_FUNC_DEF(vminsx);
+TRANS_FUNC_DEF(vblendpd);
+TRANS_FUNC_DEF(vblendps);
+TRANS_FUNC_DEF(vblendvpd);
+TRANS_FUNC_DEF(vblendvps);
+TRANS_FUNC_DEF(vbroadcastf128);
+TRANS_FUNC_DEF(vbroadcasti128);
+TRANS_FUNC_DEF(vbroadcastsd);
+TRANS_FUNC_DEF(vbroadcastss);
+TRANS_FUNC_DEF(vextractf128);
+TRANS_FUNC_DEF(vextracti128);
+TRANS_FUNC_DEF(vextractps);
+TRANS_FUNC_DEF(vinsertf128);
+TRANS_FUNC_DEF(vinserti128);
+TRANS_FUNC_DEF(vinsertps);
+TRANS_FUNC_DEF(vshufpd);
+TRANS_FUNC_DEF(vshufps);
+TRANS_FUNC_DEF(vunpckhpd);
+TRANS_FUNC_DEF(vunpckhps);
+TRANS_FUNC_DEF(vunpcklpd);
+TRANS_FUNC_DEF(vunpcklps);
+TRANS_FUNC_DEF(vpslldq);
+TRANS_FUNC_DEF(vpsllx);
+TRANS_FUNC_DEF(vpsrax);
+TRANS_FUNC_DEF(vpsrldq);
+TRANS_FUNC_DEF(vpsrlx);
+TRANS_FUNC_DEF(vpcmpeqx);
+TRANS_FUNC_DEF(vpcmpgtx);
+TRANS_FUNC_DEF(vucomisd);
+TRANS_FUNC_DEF(vucomiss);
+TRANS_FUNC_DEF(vcomisd);
+TRANS_FUNC_DEF(vcomiss);
+TRANS_FUNC_DEF(vpabsx);
+TRANS_FUNC_DEF(vpackusxx);
+TRANS_FUNC_DEF(vpand);
+TRANS_FUNC_DEF(vpandn);
+TRANS_FUNC_DEF(vpblendd);
+TRANS_FUNC_DEF(vpblendvb);
+TRANS_FUNC_DEF(vpblendw);
+TRANS_FUNC_DEF(vperm2f128);
+TRANS_FUNC_DEF(vperm2i128);
+TRANS_FUNC_DEF(vpermd);
+TRANS_FUNC_DEF(vpermq);
+TRANS_FUNC_DEF(vpextrx);
+TRANS_FUNC_DEF(vpminux);
+TRANS_FUNC_DEF(vpmovsxxx);
+TRANS_FUNC_DEF(vpmovzxxx);
+TRANS_FUNC_DEF(vpmullx);
+TRANS_FUNC_DEF(vpor);
+TRANS_FUNC_DEF(vpshufb);
+TRANS_FUNC_DEF(vpsubx);
+TRANS_FUNC_DEF(vptest);
+TRANS_FUNC_DEF(vpunpckhxx);
+TRANS_FUNC_DEF(vpunpcklxx);
+TRANS_FUNC_DEF(vpxor);
+TRANS_FUNC_DEF(vcvtdq2pd);
+TRANS_FUNC_DEF(vcvtdq2ps);
+TRANS_FUNC_DEF(vcvtpd2dq);
+TRANS_FUNC_DEF(vcvtpd2ps);
+TRANS_FUNC_DEF(vcvtps2dq);
+TRANS_FUNC_DEF(vcvtps2pd);
+TRANS_FUNC_DEF(vcvtsd2ss);
+TRANS_FUNC_DEF(vcvtsi2sd);
+TRANS_FUNC_DEF(vcvtsi2ss);
+TRANS_FUNC_DEF(vcvtss2sd);
+TRANS_FUNC_DEF(vcvttpd2dq);
+TRANS_FUNC_DEF(vcvttps2dq);
+TRANS_FUNC_DEF(vfmaddxxxpd);
+TRANS_FUNC_DEF(vfmaddxxxps);
+TRANS_FUNC_DEF(vfmaddxxxsd);
+TRANS_FUNC_DEF(vfmaddxxxss);
+TRANS_FUNC_DEF(vfmaddsubxxxpd);
+TRANS_FUNC_DEF(vfmaddsubxxxps);
+TRANS_FUNC_DEF(vfmsubaddxxxpd);
+TRANS_FUNC_DEF(vfmsubaddxxxps);
+TRANS_FUNC_DEF(vfmsubxxxpd);
+TRANS_FUNC_DEF(vfmsubxxxps);
+TRANS_FUNC_DEF(vfmsubxxxsd);
+TRANS_FUNC_DEF(vfmsubxxxss);
+TRANS_FUNC_DEF(vfnmaddxxxpd);
+TRANS_FUNC_DEF(vfnmaddxxxps);
+TRANS_FUNC_DEF(vfnmaddxxxsd);
+TRANS_FUNC_DEF(vfnmaddxxxss);
+TRANS_FUNC_DEF(vfnmsubxxxpd);
+TRANS_FUNC_DEF(vfnmsubxxxps);
+TRANS_FUNC_DEF(vfnmsubxxxsd);
+TRANS_FUNC_DEF(vfnmsubxxxss);
+TRANS_FUNC_DEF(vcmppd);
+TRANS_FUNC_DEF(vcmpeqpd);
+TRANS_FUNC_DEF(vcmpltpd);
+TRANS_FUNC_DEF(vcmplepd);
+TRANS_FUNC_DEF(vcmpunordpd);
+TRANS_FUNC_DEF(vcmpneqpd);
+TRANS_FUNC_DEF(vcmpnltpd);
+TRANS_FUNC_DEF(vcmpnlepd);
+TRANS_FUNC_DEF(vcmpordpd);
+TRANS_FUNC_DEF(vcmpeq_uqpd);
+TRANS_FUNC_DEF(vcmpngepd);
+TRANS_FUNC_DEF(vcmpngtpd);
+TRANS_FUNC_DEF(vcmpfalsepd);
+TRANS_FUNC_DEF(vcmpneq_oqpd);
+TRANS_FUNC_DEF(vcmpgepd);
+TRANS_FUNC_DEF(vcmpgtpd);
+TRANS_FUNC_DEF(vcmptruepd);
+TRANS_FUNC_DEF(vcmpeq_ospd);
+TRANS_FUNC_DEF(vcmplt_oqpd);
+TRANS_FUNC_DEF(vcmple_oqpd);
+TRANS_FUNC_DEF(vcmpunord_spd);
+TRANS_FUNC_DEF(vcmpneq_uspd);
+TRANS_FUNC_DEF(vcmpnlt_uqpd);
+TRANS_FUNC_DEF(vcmpnle_uqpd);
+TRANS_FUNC_DEF(vcmpord_spd);
+TRANS_FUNC_DEF(vcmpeq_uspd);
+TRANS_FUNC_DEF(vcmpnge_uqpd);
+TRANS_FUNC_DEF(vcmpngt_uqpd);
+TRANS_FUNC_DEF(vcmpfalse_ospd);
+TRANS_FUNC_DEF(vcmpneq_ospd);
+TRANS_FUNC_DEF(vcmpge_oqpd);
+TRANS_FUNC_DEF(vcmpgt_oqpd);
+TRANS_FUNC_DEF(vcmptrue_uspd);
+TRANS_FUNC_DEF(vcmpps);
+TRANS_FUNC_DEF(vcmpeqps);
+TRANS_FUNC_DEF(vcmpltps);
+TRANS_FUNC_DEF(vcmpleps);
+TRANS_FUNC_DEF(vcmpunordps);
+TRANS_FUNC_DEF(vcmpneqps);
+TRANS_FUNC_DEF(vcmpnltps);
+TRANS_FUNC_DEF(vcmpnleps);
+TRANS_FUNC_DEF(vcmpordps);
+TRANS_FUNC_DEF(vcmpeq_uqps);
+TRANS_FUNC_DEF(vcmpngeps);
+TRANS_FUNC_DEF(vcmpngtps);
+TRANS_FUNC_DEF(vcmpfalseps);
+TRANS_FUNC_DEF(vcmpneq_oqps);
+TRANS_FUNC_DEF(vcmpgeps);
+TRANS_FUNC_DEF(vcmpgtps);
+TRANS_FUNC_DEF(vcmptrueps);
+TRANS_FUNC_DEF(vcmpeq_osps);
+TRANS_FUNC_DEF(vcmplt_oqps);
+TRANS_FUNC_DEF(vcmple_oqps);
+TRANS_FUNC_DEF(vcmpunord_sps);
+TRANS_FUNC_DEF(vcmpneq_usps);
+TRANS_FUNC_DEF(vcmpnlt_uqps);
+TRANS_FUNC_DEF(vcmpnle_uqps);
+TRANS_FUNC_DEF(vcmpord_sps);
+TRANS_FUNC_DEF(vcmpeq_usps);
+TRANS_FUNC_DEF(vcmpnge_uqps);
+TRANS_FUNC_DEF(vcmpngt_uqps);
+TRANS_FUNC_DEF(vcmpfalse_osps);
+TRANS_FUNC_DEF(vcmpneq_osps);
+TRANS_FUNC_DEF(vcmpge_oqps);
+TRANS_FUNC_DEF(vcmpgt_oqps);
+TRANS_FUNC_DEF(vcmptrue_usps);
+TRANS_FUNC_DEF(vcmpsd);
+TRANS_FUNC_DEF(vcmpeqsd);
+TRANS_FUNC_DEF(vcmpltsd);
+TRANS_FUNC_DEF(vcmplesd);
+TRANS_FUNC_DEF(vcmpunordsd);
+TRANS_FUNC_DEF(vcmpneqsd);
+TRANS_FUNC_DEF(vcmpnltsd);
+TRANS_FUNC_DEF(vcmpnlesd);
+TRANS_FUNC_DEF(vcmpordsd);
+TRANS_FUNC_DEF(vcmpeq_uqsd);
+TRANS_FUNC_DEF(vcmpngesd);
+TRANS_FUNC_DEF(vcmpngtsd);
+TRANS_FUNC_DEF(vcmpfalsesd);
+TRANS_FUNC_DEF(vcmpneq_oqsd);
+TRANS_FUNC_DEF(vcmpgesd);
+TRANS_FUNC_DEF(vcmpgtsd);
+TRANS_FUNC_DEF(vcmptruesd);
+TRANS_FUNC_DEF(vcmpeq_ossd);
+TRANS_FUNC_DEF(vcmplt_oqsd);
+TRANS_FUNC_DEF(vcmple_oqsd);
+TRANS_FUNC_DEF(vcmpunord_ssd);
+TRANS_FUNC_DEF(vcmpneq_ussd);
+TRANS_FUNC_DEF(vcmpnlt_uqsd);
+TRANS_FUNC_DEF(vcmpnle_uqsd);
+TRANS_FUNC_DEF(vcmpord_ssd);
+TRANS_FUNC_DEF(vcmpeq_ussd);
+TRANS_FUNC_DEF(vcmpnge_uqsd);
+TRANS_FUNC_DEF(vcmpngt_uqsd);
+TRANS_FUNC_DEF(vcmpfalse_ossd);
+TRANS_FUNC_DEF(vcmpneq_ossd);
+TRANS_FUNC_DEF(vcmpge_oqsd);
+TRANS_FUNC_DEF(vcmpgt_oqsd);
+TRANS_FUNC_DEF(vcmptrue_ussd);
+TRANS_FUNC_DEF(vcmpss);
+TRANS_FUNC_DEF(vcmpeqss);
+TRANS_FUNC_DEF(vcmpltss);
+TRANS_FUNC_DEF(vcmpless);
+TRANS_FUNC_DEF(vcmpunordss);
+TRANS_FUNC_DEF(vcmpneqss);
+TRANS_FUNC_DEF(vcmpnltss);
+TRANS_FUNC_DEF(vcmpnless);
+TRANS_FUNC_DEF(vcmpordss);
+TRANS_FUNC_DEF(vcmpeq_uqss);
+TRANS_FUNC_DEF(vcmpngess);
+TRANS_FUNC_DEF(vcmpngtss);
+TRANS_FUNC_DEF(vcmpfalsess);
+TRANS_FUNC_DEF(vcmpneq_oqss);
+TRANS_FUNC_DEF(vcmpgess);
+TRANS_FUNC_DEF(vcmpgtss);
+TRANS_FUNC_DEF(vcmptruess);
+TRANS_FUNC_DEF(vcmpeq_osss);
+TRANS_FUNC_DEF(vcmplt_oqss);
+TRANS_FUNC_DEF(vcmple_oqss);
+TRANS_FUNC_DEF(vcmpunord_sss);
+TRANS_FUNC_DEF(vcmpneq_usss);
+TRANS_FUNC_DEF(vcmpnlt_uqss);
+TRANS_FUNC_DEF(vcmpnle_uqss);
+TRANS_FUNC_DEF(vcmpord_sss);
+TRANS_FUNC_DEF(vcmpeq_usss);
+TRANS_FUNC_DEF(vcmpnge_uqss);
+TRANS_FUNC_DEF(vcmpngt_uqss);
+TRANS_FUNC_DEF(vcmpfalse_osss);
+TRANS_FUNC_DEF(vcmpneq_osss);
+TRANS_FUNC_DEF(vcmpge_oqss);
+TRANS_FUNC_DEF(vcmpgt_oqss);
+TRANS_FUNC_DEF(vcmptrue_usss);
+TRANS_FUNC_DEF(vpbroadcastq);
+TRANS_FUNC_DEF(vpaddq);
+TRANS_FUNC_DEF(vzeroupper);
+
+//TRANS_FUNC_DEF(vpinsrb);
+//TRANS_FUNC_DEF(vpinsrq);
+TRANS_FUNC_DEF(xgetbv);
+TRANS_FUNC_DEF(xsetbv);
+TRANS_FUNC_DEF(xsave);
+TRANS_FUNC_DEF(xsaveopt);
+TRANS_FUNC_DEF(xrstor);
+TRANS_FUNC_DEF(xgetbv_wrap);
+TRANS_FUNC_DEF(xsetbv_wrap);
+TRANS_FUNC_DEF(xsave_wrap);
+TRANS_FUNC_DEF(xsaveopt_wrap);
+TRANS_FUNC_DEF(xrstor_wrap);
+#endif
+
 TRANS_FUNC_DEF(andn);
 TRANS_FUNC_DEF(movbe);
 TRANS_FUNC_DEF(rorx);
+//TRANS_FUNC_DEF(vpmaxsd);
+//TRANS_FUNC_DEF(vpminsd);
 TRANS_FUNC_DEF(blsi);
+
+#ifdef CONFIG_LATX_AVX_OPT
+TRANS_FUNC_DEF(vpcmpestri);
+#else
 TRANS_FUNC_DEF(pcmpestri);
+#endif
+
+#ifdef CONFIG_LATX_AVX_OPT
+TRANS_FUNC_DEF(vpackssxx);
+TRANS_FUNC_DEF(vpshufhw);
+TRANS_FUNC_DEF(vpshuflw);
+TRANS_FUNC_DEF(vpavgb);
+TRANS_FUNC_DEF(vpavgw);
+TRANS_FUNC_DEF(vdppd);
+TRANS_FUNC_DEF(vdpps);
+TRANS_FUNC_DEF(vmpsadbw);
+TRANS_FUNC_DEF(vphaddw);
+TRANS_FUNC_DEF(vphaddd);
+TRANS_FUNC_DEF(vphaddsw);
+TRANS_FUNC_DEF(vpsadbw);
+TRANS_FUNC_DEF(vroundps);
+TRANS_FUNC_DEF(vroundss);
+TRANS_FUNC_DEF(vroundpd);
+TRANS_FUNC_DEF(vroundsd);
+#endif
 TRANS_FUNC_DEF(pcmpestrm);
+
+#ifdef CONFIG_LATX_AVX_OPT
+TRANS_FUNC_DEF(vpcmpistri);
+#else
 TRANS_FUNC_DEF(pcmpistri);
+#endif
+
 TRANS_FUNC_DEF(pcmpistrm);
+#ifdef CONFIG_LATX_AVX_OPT
+TRANS_FUNC_DEF(vpcmpestrm);
+TRANS_FUNC_DEF(vpcmpistrm);
+#endif
 TRANS_FUNC_DEF(aesdec);
 TRANS_FUNC_DEF(aesdeclast);
 TRANS_FUNC_DEF(aesenc);
 TRANS_FUNC_DEF(aesenclast);
 TRANS_FUNC_DEF(aesimc);
 TRANS_FUNC_DEF(aeskeygenassist);
+
+#ifdef CONFIG_LATX_AVX_OPT
+TRANS_FUNC_DEF(vaesdec);
+TRANS_FUNC_DEF(vaesdeclast);
+TRANS_FUNC_DEF(vaesenc);
+TRANS_FUNC_DEF(vaesenclast);
+TRANS_FUNC_DEF(vaesimc);
+TRANS_FUNC_DEF(vaeskeygenassist);
+TRANS_FUNC_DEF(vpsrlvd);
+TRANS_FUNC_DEF(vpsrlvq);
+TRANS_FUNC_DEF(vpgatherdd);
+TRANS_FUNC_DEF(vpgatherqd);
+TRANS_FUNC_DEF(vpgatherdq);
+TRANS_FUNC_DEF(vpgatherqq);
+#endif
 
 TRANS_FUNC_DEF(pext);
 TRANS_FUNC_DEF(pdep);
@@ -734,6 +1130,13 @@ TRANS_FUNC_DEF(bzhi);
 TRANS_FUNC_DEF(lzcnt);
 TRANS_FUNC_DEF(adcx);
 TRANS_FUNC_DEF(adox);
+
+#ifdef CONFIG_LATX_AVX_OPT
+TRANS_FUNC_DEF(vpbroadcastd);
+TRANS_FUNC_DEF(vpbroadcastb);
+TRANS_FUNC_DEF(vpbroadcastw);
+#endif
+
 TRANS_FUNC_DEF(crc32);
 TRANS_FUNC_DEF(salc);
 TRANS_FUNC_DEF(pclmulqdq);

target/i386/latx/ir1/ir1.c
@@ -334,6 +334,12 @@ ADDRX ir1_disasm(IR1_INST *ir1, uint8_t *addr, ADDRX t_pc, int ir1_num, void *pi
         /* repleace rdsspq with 5 bytes nop, just a temporary solution */
         addr = (uint8_t *)&nop_5;
     }
+#ifdef CONFIG_LATX_AVX_OPT
+    if (((*((uint64_t *)addr)) & 0xfffffaff) == 0xae0f48f3) {
+        /* repleace incsspq with 5 bytes nop, just a temporary solution */
+        addr = (uint8_t *)&nop_5;
+    }
+#endif
     /* FIXME:the count parameter in cs_disasm is 1, it means we translte 1 insn at a time,
      * there should be a performance improvement if we increase the number, but
      * for now there are some problems if we change it. It will be settled later.

target/i386/latx/translator/meson.build
@@ -20,7 +20,9 @@ i386_ss.add(when: 'CONFIG_LATX', if_true: files(
   'tr-simd-cvt.c',
   'tr-simd-mov.c',
   'tr-simd-shift.c',
+  'tr-simd-fma.c',
   'tr-simd-sha.c',
+  'tr-simd-f16c.c',
   'tr-bmi.c',
   'tr-adx.c',
   'tr-softfpu.c',

commit 920ce55c65a56bfa53972059162be246da8d619d

LATX, fix: update mapped EAX/EDX after softfpu XGETBV
The softfpu XGETBV translator split the helper return value and stored
it only in the EAX and EDX slots in CPUX86State. During translated
execution, however, guest GPRs are held in fixed mapped LoongArch
registers. Subsequent guest instructions therefore continued to read
stale EAX and EDX values from s0 and s2.

This could cause guest AVX availability checks to fail even when XCR0
had both the SSE and AVX state components enabled.

Write the low and high 32-bit halves of the helper return value directly
to the mapped EAX and EDX operands, matching the non-softfpu XGETBV
path. The values will be saved to CPUX86State at the normal explicit
state synchronization points.

Fixes: fe5993f639f6 ("Add avx support")

Signed-off-by: Zewei Yang <yangzewei@loongson.cn>

target/i386/latx/translator/tr-softfpu.c
@@ -3089,12 +3089,12 @@ static bool translate_xgetbv_softfpu(IR1_INST *pir1)
 {
     IR2_OPND ecx_opnd = ra_alloc_gpr(ecx_index);
     gen_softfpu_helper2m_ptr((ADDR)helper_xgetbv, ecx_opnd);
-    IR2_OPND temp_low = ra_alloc_itemp();
-    IR2_OPND temp_high = ra_alloc_itemp();
-    la_bstrpick_d(temp_low, a0_ir2_opnd, 31, 0);
-    la_bstrpick_d(temp_high, a0_ir2_opnd, 63, 32);
-    la_st_d(temp_low, env_ir2_opnd, lsenv_offset_of_gpr(lsenv, R_EAX));
-    la_st_d(temp_high, env_ir2_opnd, lsenv_offset_of_gpr(lsenv, R_EDX));
+    
+    IR2_OPND eax_opnd = ra_alloc_gpr(eax_index);
+    IR2_OPND edx_opnd = ra_alloc_gpr(edx_index);
+
+    la_bstrpick_d(eax_opnd, a0_ir2_opnd, 31, 0);
+    la_bstrpick_d(edx_opnd, a0_ir2_opnd, 63, 32);
     return true;
 }
 